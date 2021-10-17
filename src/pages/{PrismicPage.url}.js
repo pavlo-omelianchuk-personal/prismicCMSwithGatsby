454b7fa89b12e91778fd7df2f3f88aa1e83f9c30
Pavlo Omelianchuk <pavlotvingo@gmail.com>
2021-10-17 22:26:42 +0100

    Add dynamic pages

---
+++ src/pages/{PrismicPage.url}.js
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+
+import { Layout } from '../components/Layout'
+import { Seo } from '../components/Seo'
+import { SliceZone } from '../components/SliceZone'
+
+const PageTemplate = ({ data }) => {
+  if (!data) return null
+  const doc = data.prismicPage
+console.log(data)
+  return (
+    <Layout>
+      <Seo title={doc.data.document_display_name.text} />
+      <SliceZone sliceZone={doc.data.body} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PageQuery($id: String) {
+    prismicPage(id: { eq: $id }) {
+      data {
+        document_display_name {
+          text
+        }
+        body {
+          ... on PrismicSliceType {
+            slice_type
+          }
+          ...PageDataBodyText
+          ...PageDataBodyQuote
+          ...PageDataBodyFullWidthImage
+          ...PageDataBodyImageGallery
+          ...PageDataBodyImageHighlight
+        }
+      }
+    }
+  }
+`
+
+export default PageTemplate
